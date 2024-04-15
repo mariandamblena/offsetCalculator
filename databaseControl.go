dc41bfc7c2f4bfa67730ecda6916cafa476ab479
mariandamblena <marianodambo@hotmail.com>
2024-04-15 15:30:39 -0300

    Configuracion de puertos y regla tcp

--- databaseControl.go
+++ databaseControl.go
@@ -6,8 +6,9 @@ import (
 )
 
 var (
-	server = flag.String("server", "127.0.0.1", "the database server")
-	port   = flag.Int("port", 1434, "the database port")
+	server   = flag.String("server", "127.0.0.1", "the database server")
+	port     = flag.Int("port", 1433, "the database port")
+	database = flag.String("database", "offsetRecorder", "the database name")
 	/*
 		No se usan porque ingresamos con la autentifiacion de windows
 		////////////////////////////////////////////////////////////////
@@ -22,7 +23,7 @@ func parseFlags() {
 }
 
 func connectionString() string {
-	return fmt.Sprintf("server=%s;integrated security=true;port=%d", *server, *port)
+	return fmt.Sprintf("server=%s;database=%s;integrated security=true;port=%d", *server, *database, *port)
 }
 
 /*
@@ -30,6 +31,6 @@ Se usa en caso de autentificarse con usuario
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 func connectionString() string {
-	return fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d", *server, *user, *password, *port)
+	return fmt.Sprintf("server=%s;database=%s;user id=%s;password=%s;port=%d", *server, *database, *user, *password, *port)
 }
 */
